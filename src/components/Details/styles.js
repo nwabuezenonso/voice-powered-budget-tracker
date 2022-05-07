9966996a5a7871dca2a63595ed2234d8c3233978
stephen <nwabuezesc@gmail.com>
2022-05-07 06:01:35 +0100

    fixing packages

--- src/components/Details/styles.js
+++ src/components/Details/styles.js
@@ -1,5 +1,7 @@
+// import make styles from component that allow us create our styles
 import { makeStyles } from '@material-ui/core/styles';
 
+// exorting styles component
 export default makeStyles(() => ({
   income: {
     borderBottom: '10px solid rgba(0, 255, 0, 0.5)',
